8f715de4638331f8f3ea80bbf1aac01241e6003a
mozcelikors <mozcelikors@gmail.com>
2017-12-17 12:50:46 +0100

    New changes
    Signed-off-by: mozcelikors <mozcelikors@gmail.com>

--- scripts/start_rovertelemetryui.js
+++ scripts/start_rovertelemetryui.js
@@ -1,3 +1,17 @@
+/*
+ Copyright (c) 2017 FH Dortmund.
+ All rights reserved. This program and the accompanying materials
+ are made available under the terms of the Eclipse Public License v1.0
+ which accompanies this distribution, and is available at
+ http://www.eclipse.org/legal/epl-v10.html
+
+ Description:
+    rover-telemetry-ui | Express web server, socket.io server, and MQTT module
+
+ Designed and realized by:
+    M. Ozcelikors <mozcelikors@gmail.com>
+*/
+
 // Setup basic express server
 var express = require('express');
 var app = express();
@@ -13,63 +27,16 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(path.join(__dirname, '..')));
 
-/*
-var numUsers = 0;
 
 io.on('connection', function (socket) {
-	var addedUser = false;
-	
-	// when the client emits 'new message', this listens and executes
-	socket.on('new message', function (data) {
-		// we tell the client to execute 'new message'
-		socket.broadcast.emit('new message', {
+
+	/* Control data received */
+	socket.on('control', function (data) {
+		console.log('Data received', data);
+		/*socket.broadcast.emit('new message', {
 			username: socket.username,
 			message: data
-		});
-	});
-
-	// when the client emits 'add user', this listens and executes
-	socket.on('add user', function (username) {
-		if (addedUser) return;
-	
-		// we store the username in the socket session for this client
-		socket.username = username;
-		++numUsers;
-		addedUser = true;
-		socket.emit('login', {
-			numUsers: numUsers
-		});
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('user joined', {
-			username: socket.username,
-			numUsers: numUsers
-		});
+		});*/
 	});
 	
-	// when the client emits 'typing', we broadcast it to others
-	socket.on('typing', function () {
-			socket.broadcast.emit('typing', {
-			username: socket.username
-		});
-	});
-	
-	// when the client emits 'stop typing', we broadcast it to others
-	socket.on('stop typing', function () {
-			socket.broadcast.emit('stop typing', {
-			username: socket.username
-		});
-	});
-
-	// when the user disconnects.. perform this
-	socket.on('disconnect', function () {
-		if (addedUser) {
-			--numUsers;
-		
-			// echo globally that this client has left
-			socket.broadcast.emit('user left', {
-				username: socket.username,
-				numUsers: numUsers
-			});
-		}
-	});
-});*/
+});
